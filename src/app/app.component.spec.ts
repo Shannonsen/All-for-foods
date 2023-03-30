9dc72a529dd488908798185ec27d6b504283c32a
Shannonsen <a18016307@alumnos.uady.mx>
2023-03-30 14:49:39 -0600

    feature: change stylesheet format css to scss

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'all-for-foods'`, () => {
+  it(`should have as title 'angular-scss'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('all-for-foods');
+    expect(app.title).toEqual('angular-scss');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('all-for-foods app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-scss app is running!');
   });
 });
