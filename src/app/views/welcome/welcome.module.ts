eac2a5b5daf17ce3e9b64e43a5679c47adde4dea
Shannonsen <a18016307@alumnos.uady.mx>
2023-04-05 16:14:36 -0600

    feature: refactor home

--- src/app/views/welcome/welcome.module.ts
+++ src/app/views/welcome/welcome.module.ts
@@ -4,11 +4,16 @@ import { CommonModule } from '@angular/common';
 import { WelcomeRoutingModule } from './welcome-routing.module';
 import { HomeComponent } from './pages/home/home.component';
 import { SharedModule } from '../shared/shared.module';
+import { ListProductsComponent } from './components/list-products/list-products.component';
+import { FrontImageComponent } from './components/front-image/front-image.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
+    ListProductsComponent,
+    ListProductsComponent,
+    FrontImageComponent,
   ],
   imports: [
     CommonModule,
