bffb98cfc71b7056d69ebf10f980b6133bd3ace4
Alexandra Oreza <A18016321@alumnos.uady.mx>
2023-04-09 10:32:23 -0600

    changed display of list

--- src/app/views/user-info/components/fav-list/fav-list.component.ts
+++ src/app/views/user-info/components/fav-list/fav-list.component.ts
@@ -9,8 +9,30 @@ import { Food } from 'src/app/views/shared/models/food.model';
 export class FavListComponent implements OnInit {
   @Input() favs: Food[] = []
 
+  currentPage: number = 1;
+  pageSize: number = 3;
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  get paginatedFavs() {
+    const startIndex = (this.currentPage - 1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    return this.favs.slice(startIndex, endIndex);
+  }
+
+  totalPagesArray(): number[] {
+    const pageCount = Math.ceil(this.favs.length / this.pageSize); // total number of pages
+    return Array.from({ length: pageCount }, (_, i) => i + 1); // create an array of page numbers
+  }
+
+  loadPage(pageNumber: number): Food[] {
+    const startIndex = (pageNumber - 1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize
+    return this.favs.slice(startIndex, endIndex);
+  }
+  
 }
