553ab52da1469d706439cfff67543123315c8658
Shannonsen <a18016307@alumnos.uady.mx>
2023-04-10 18:50:25 -0600

    fix: change color star yellow to gold

--- src/app/views/shared/components/list-products/list-products.component.ts
+++ src/app/views/shared/components/list-products/list-products.component.ts
@@ -16,24 +16,24 @@ export class ListProductsComponent implements OnInit {
 
   changeColor(id: number) {
     const heart = document.getElementById(String(id))!;
-    if(heart.style.color=="red"){
-      heart.style.color="gray";
-      heart.style.scale="1";
-    }else{
-      heart.style.color="red";
-      heart.style.scale="1.2";
+    if (heart.style.color == "red") {
+      heart.style.color = "gray";
+      heart.style.scale = "1";
+    } else {
+      heart.style.color = "red";
+      heart.style.scale = "1.2";
     }
   }
 
-  changeRating(idStar:number, idProduct:number){
+  changeRating(idStar: number, idProduct: number) {
 
-    const star = document.getElementById(String("star-"+ idStar + "-"+ idProduct))!;
-    if(star.style.color=="#F4DF21"){
-      star.style.color="gray";
-      star.style.scale="1";
-    }else{
-      star.style.color="#F4DF21";
-      star.style.scale="1.2";
+    const star = document.getElementById(String("star-" + idStar + "-" + idProduct))!;
+    if (star.style.color == "gold") {
+      star.style.color = "gray";
+      star.style.scale = "1";
+    } else {
+      star.style.color = "gold";
+      star.style.scale = "1.2";
     }
 
   }
