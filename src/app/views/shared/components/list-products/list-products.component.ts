13281d02d9d00b8e79490231857dbd3a9ec115aa
Shannonsen <a18016307@alumnos.uady.mx>
2023-04-10 18:34:30 -0600

    feature: heart reaction product

--- src/app/views/shared/components/list-products/list-products.component.ts
+++ src/app/views/shared/components/list-products/list-products.component.ts
@@ -9,10 +9,20 @@ import { Food } from 'src/app/views/shared/models/food.model';
 export class ListProductsComponent implements OnInit {
 
   @Input() products: Food[] = [];
-
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changeColor(id: number) {
+    const heart = document.getElementById(String(id))!;
+    if(heart.style.color=="red"){
+      heart.style.color="gray";
+      heart.style.scale="1";
+    }else{
+      heart.style.color="red";
+      heart.style.scale="1.2";
+    }
   }
 
 }
