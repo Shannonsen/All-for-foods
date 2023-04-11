e8cad9e5c505081f8ac15e077aa0ed32bc3dbdea
AlexandraOreza <58123582+AlexandraOreza@users.noreply.github.com>
2023-04-10 22:53:30 -0600

    Revert "Aff 07/fix/user info"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,39 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  {
-    path: 'home', loadChildren: () => import('./views/welcome/welcome.module').then(m => m.WelcomeModule)
-  },
-  {
-    path: 'profile', loadChildren: () => import('./views/user-info/user-info.module').then(m => m.UserInfoModule)
-  },
-  {
-    path: 'dashboard', loadChildren: () => import('./views/dashboard/dashboard.module').then(m => m.DashboardModule)
-  },
-  {
-    path: 'register', loadChildren: () => import('./views/register/register.module').then(m => m.RegisterModule)
-  },
-  {
-    path: 'login', loadChildren: () => import('./views/login/login.module').then(m => m.LoginModule)
-  },
-  {
-    path: 'recipe', loadChildren: () => import('./views/recipe/recipe.module').then(m => m.RecipeModule)
-  },
-  {
-    path: 'support', loadChildren: () => import('./views/support/support.module').then(m => m.SupportModule)
-  },
-  {
-    path: 'recipes', loadChildren: () => import('./views/recipes/recipes.module').then(m => m.RecipesModule)
-  },
-  {
-    path: 'search', loadChildren: () => import('./views/search/search.module').then(m => m.SearchModule)
-  },
-  {
-    path: '**', loadChildren: () => import('./views/notfound/notfound.module').then(m => m.NotfoundModule)
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from './views/shared/shared.module';
-
-import { HttpClientModule } from '@angular/common/http';
-import { TagInputComponent } from './views/shared/components/tag-input/tag-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    SharedModule,
-    HttpClientModule,
-    BrowserAnimationsModule
+    AppRoutingModule
   ],
-  providers: [TagInputComponent],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
