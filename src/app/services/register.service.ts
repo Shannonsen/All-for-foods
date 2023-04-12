2646e69a6c48fe435277755913b5d8a61f3a7b60
Noeshi <A18016311@alumnos.uady.mx>
2023-04-11 21:02:34 -0600

    documentation services

--- src/app/services/register.service.ts
+++ src/app/services/register.service.ts
@@ -1,16 +1,24 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-
+/**
+ * Clase que representa el servicio de registro
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
 
   private LOCAL_SUCCESS = 'http://localhost:4200/assets/success_post.json';
-
+  /**
+   * @constructor
+   * @param {HttpClient} http : Cliente http 
+   */
   constructor(private http: HttpClient) { }
-
+  /**
+   * Método para el registro de un usuario
+   * @returns {Observable<ay>} Lista de usuarios
+   */
   public registerUser(): Observable<any>{
     return this.http.get(this.LOCAL_SUCCESS);
   }
