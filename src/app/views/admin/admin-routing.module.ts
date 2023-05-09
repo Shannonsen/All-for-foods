d75324899c790a59b4b089b3ef6c490b4c4393f8
Shannonsen <a18016307@alumnos.uady.mx>
2023-05-09 14:52:51 -0600

    feature: create route panel if the user is admin

--- src/app/views/admin/admin-routing.module.ts
+++ src/app/views/admin/admin-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PanelComponent } from './pages/panel/panel.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: PanelComponent }
+];
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
