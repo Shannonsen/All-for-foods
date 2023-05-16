5a28a6105824afb72db4a567a96df09a72ee5f33
Shannonsen <a18016307@alumnos.uady.mx>
2023-05-16 16:25:02 -0600

    fix: clean code fav-list and my-recipe-list

--- src/app/views/user-info/components/fav-list/fav-list.component.ts
+++ src/app/views/user-info/components/fav-list/fav-list.component.ts
@@ -10,8 +10,6 @@ import { RecipesService } from 'src/app/services/recipes.service';
   styleUrls: ['./fav-list.component.scss']
 })
 export class FavListComponent implements OnInit {
-  @Input() favorites: number[] = []
-  favs: Food[] = [];
   @Input() foods: Food[] = [];
   @Input() typeSearch:string = "favorites"
   @Input() isPanel:string = ""
@@ -22,34 +20,16 @@ export class FavListComponent implements OnInit {
   totalPages: number[] = [];
   sectionName: string = 'Mis Favoritos';
 
-  /**
-   * @constructor
-   * @param recipeService : Servicio de recetas.
-   */
-  constructor(private recipeService: RecipesService) { }
+/**
+ * @constructor
+ */
+  constructor() { }
 
   /**
    * @override
    */
   ngOnInit(): void {
-    this.recipeService.getAllFoods().subscribe(recipes => {
-      this.favs = (recipes as Food[]).filter(p => this.favorites.includes(p.id));
-
-
-    const pageCount = Math.ceil(this.favorites.length / this.pageSize);
-    this.totalPages = Array.from({ length: pageCount }, (_, i) => i + 1);
-    })
   }
 
-  /**
-   * Método que regresa el listado de recetas a mostrar en el paginado actual de la lista.
-   * @param {number} pageNumber : El valor del paginado actual.
-   * @returns {Food[]} : Regresa un listado de recetas.
-   */
-  loadPage(pageNumber: number): Food[] {
-    const startIndex = (pageNumber - 1) * this.pageSize;
-    const endIndex = startIndex + this.pageSize
-    return this.favs.slice(startIndex, endIndex);
-  }
 }
 
