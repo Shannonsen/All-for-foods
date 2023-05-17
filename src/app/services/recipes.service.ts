ae09b7fdbd0d4d26aa5d7b20eca6f489e9c1793c
Alexandra Oreza <A18016321@alumnos.uady.mx>
2023-05-16 22:23:58 -0600

    missing comment in RecipeService, - unused methods

--- src/app/services/recipes.service.ts
+++ src/app/services/recipes.service.ts
@@ -20,6 +20,7 @@ export class RecipesService {
 
   /**
    * Método que obtiene todas las recetas.
+   * 
    * @returns {Observable<any>} La lista de todas las recetas.
    */
   public getAllFoods(page: number = 1 , size:number = 4): Observable<any> {
@@ -29,6 +30,13 @@ export class RecipesService {
     return this.http.get("http://localhost:3001/api/v1/recipe/",{params});
   }
 
+  /**
+   * Método que obtiene todas las recetas activas
+   * 
+   * @param token : El token de autorización para autenticación
+   * @param page : El número de página para la paginación (default 2)
+   * @returns {Observable<any>} Array de recetas activas
+   */
   public getAllRecipesActivated(token: string, page:number = 2): Observable<any> {
     const params = new HttpParams()
     .set('page', page)
@@ -36,6 +44,13 @@ export class RecipesService {
     return this.http.get('http://localhost:3001/api/v1/recipe/admin/getAll/1', {'headers': headers, 'params': params});
   }
 
+  /**
+   * Método que obtiene todas las recetas desactivadas
+   * 
+   * @param token : El token de autorización para autenticación
+   * @param page : El número de página para la paginación (default 2)
+   * @returns {Observable<any>} Array de recetas desactivadas
+   */
   public getAllRecipesDesactivated(token: string, page:number = 2): Observable<any> {
     const params = new HttpParams()
     .set('page', page)
@@ -43,34 +58,73 @@ export class RecipesService {
     return this.http.get('http://localhost:3001/api/v1/recipe/admin/getAll/0', {'headers': headers, 'params': params});
   }
 
+  /**
+   * Método que obtiene todas las recetas de los foodies que sigue el usuario
+   * @param id : El id del usuario
+   * @param page : El número de página para la paginación (default 1)
+   * @returns {Observable<any>} Array de recetas de los foodies que sigue el usuario
+   */
   public getAllFollowsRecipes(id:number, page: number = 1): Observable<any> {
     const params = new HttpParams()
     .set('page', page)
     return this.http.get("http://localhost:3001/api/v1/recipe/myFollows/" + id, {'params': params})
   }
 
+  /**
+   * Método que elimina (desactiva) una receta con base en su id
+   * 
+   * @param idRecipe : El id de la receta a eliminar (desactivar)
+   * @param token : El token de autorización para autenticación
+   * @returns {Observable<any>} El resultado de la eliminación (desactivación)
+   */
   public deleteRecipe(idRecipe: number, token: string): Observable<any>{
     const headers = new HttpHeaders({'authorization': token});
     return this.http.put("http://localhost:3001/api/v1/recipe/delete/" + idRecipe, null,  {'headers': headers});
   }
 
+  /**
+   * Método que reactiva una receta con base en su id
+   * 
+   * @param idRecipe : El id de la receta a reactivar
+   * @param token : El token de autorización para autenticación
+   * @returns {Observable<any>} El resultado de la reactivación
+   */
   public activeRecipe(idRecipe: number, token: string): Observable<any>{
     const headers = new HttpHeaders({'authorization': token});
     return this.http.put("http://localhost:3001/api/v1/recipe/reactivate/" + idRecipe, null,  {'headers': headers});
   }
 
+  /**
+   * Método que obtiene las recetas favoritas de un usuario, paginadas
+   * 
+   * @param id : El id del usuario
+   * @param page : El número de página para la paginación
+   * @returns {Observable<any>} Array con las recetas favoritas del usuario
+   */
   public getMyFavoritesRecipes(id:number, page: number): Observable<any>{
     const params = new HttpParams()
     .set('page', page)
     return this.http.get("http://localhost:3001/api/v1/favorite/" + id, {'params': params})
   }
 
+  /**
+   * Método que obtiene todas las recetas favoritas de un usuario
+   * 
+   * @param id : El id del usuario
+   * @returns {Observable<any>} Array con las recetas favoritas del usuario
+   */
   public getAllMyFavoritesRecipes(id: number): Observable<any>{
     const params = new HttpParams()
     .set('size', '*')
     return this.http.get("http://localhost:3001/api/v1/favorite/" + id, {'params': params})
   }
 
+  /**
+   * Método que obtiene las recetas que le pertenecen a un usuario
+   * @param id : El id del usuario
+   * @param page : El número de página para la paginación
+   * @returns {Observable<any>} Array con las recetas del usuario
+   */
   public getMyRecipes(id: number, page:number): Observable<any>{
     const params = new HttpParams()
     .set('page', page)
@@ -79,23 +133,47 @@ export class RecipesService {
 
   /**
    * Método que devuelve la información de una receta por su id.
+   * 
    * @param {number} id : Identificador único de la receta.
-   * @returns {Observable <Food | undefined>} : Regresa la receta encontrada que corresponde al id.
+   * @returns {Observable<any>} : Regresa la receta encontrada que corresponde al id.
    */
   public getRecipeById(id: number): Observable<any> {
     return this.http.get<Food>("http://localhost:3001/api/v1/recipe/"+ id);
   }
 
+  /**
+   * Método que actualiza una receta con base en su id
+   * 
+   * @param id : El id de la receta
+   * @param token : El token de autorización para autenticación
+   * @param body : La información a actualizar de la receta
+   * @returns {Observable<any>} El resultado de la actualización de la receta
+   */
   public putRecipeById(id: number, token: string, body: any): Observable<any>{
     const headers = new HttpHeaders({'authorization': token});
     return this.http.put("http://localhost:3001/api/v1/recipe/" + id, body , {'headers': headers})
   }
 
+  /**
+   * Método que crea una receta
+   * 
+   * @param token : El token de autorización para autenticación
+   * @param body : La información de la receta a crear
+   * @returns {Observable<any>} El resultado de la creación de la receta
+   */
   public postRecipe(token: string, body: any): Observable<any>{
     const headers = new HttpHeaders({'authorization': token});
     return this.http.post("http://localhost:3001/api/v1/recipe", body, {'headers': headers})
   }
 
+  /**
+   * Método que devuelve si las recetas especificadas son favoritas del usuario
+   * 
+   * @param recipeId : Array de ids de las recetas a checar
+   * @param userId : El id del usuario
+   * @param token : El token de autorización para autenticación
+   * @returns {Observable<any>} El resultado de la comprobación
+   */
   public isFavorite(recipeId: number[], userId: number, token: string): Observable<any>{
     let body ={
       "userId": userId,
@@ -105,11 +183,27 @@ export class RecipesService {
     return this.http.post("http://localhost:3001/api/v1/favorite/byRecipesIds", body, {'headers': headers})
   }
 
+  /**
+   * Método que elimina una receta de la lista de favoritos con base en su id
+   * 
+   * @param recipeId : El id de la receta a eliminar de favoritos
+   * @param userId : El id del usuario
+   * @param token : El token de autorización para autenticación
+   * @returns {Observable<any>} El resultado de la eliminación
+   */
   public deleteFavorite(recipeId: number, userId: number, token: string): Observable<any>{
     const headers = new HttpHeaders({'authorization': token})
     return this.http.delete("http://localhost:3001/api/v1/favorite/" + userId + "/" + recipeId, {'headers': headers})
   }
 
+  /**
+   * Método que agrega una receta a la lista de favoritos del usuario
+   * 
+   * @param recipeId : El id de la receta
+   * @param userId : El id del usuario
+   * @param token : El token de autorización para autenticación
+   * @returns {Observable<any>} El resultado de la adición a favoritos del usuario
+   */
   public postFavorite(recipeId: number, userId: number, token: string): Observable<any>{
     let body ={
       "userId": userId,
@@ -119,17 +213,42 @@ export class RecipesService {
     return this.http.post("http://localhost:3001/api/v1/favorite/", body, {'headers': headers})
   }
 
+  /**
+   * Método que devuelve la calificación dada a una receta por un usuario
+   * 
+   * @param token : El token de autorización para autenticación
+   * @param idUser : El id del usuario
+   * @param idRecipe : El id de la receta
+   * @returns {Observable<any>} La calificación de la receta dada por el usuario
+   */
   public getMyScore(token: string, idUser: number, idRecipe: number): Observable<any>{
     const headers = new HttpHeaders({'authorization': token});
     return this.http.get("http://localhost:3001/api/v1/score/"+ idUser +"/" + idRecipe, {'headers': headers})
   }
 
+  /**
+   * Método que elimina la calificación dada a una receta por un usuario
+   * 
+   * @param token : El token de autorización para autenticación
+   * @param idUser : El id del usuario
+   * @param idRecipe : El id de la receta
+   * @returns {Observable<any>} El resultado de la eliminación
+   */
   public deleteMyScore(token: string,idUser: number, idRecipe: number ):Observable<any>{
     const headers = new HttpHeaders({'authorization': token});
     return this.http.delete("http://localhost:3001/api/v1/score/" + idUser + "/" + idRecipe,  {'headers': headers})
 
   }
 
+  /**
+   * Método que guarda la calificación dada a una receta por un usuario
+   * 
+   * @param token : El token de autorización para autenticación
+   * @param idUser : El id del usuario
+   * @param idRecipe : El id de la receta
+   * @param score : La calificación a guardar
+   * @returns {Observable<any>} El resultado de la calificación guardada
+   */
   public postMyScore(token: string,idUser: number, idRecipe: number, score: number):Observable<any>{
     let body ={
       "userId": idUser,
@@ -140,6 +259,12 @@ export class RecipesService {
     return this.http.post("http://localhost:3001/api/v1/score/", body, {'headers': headers})
   }
 
+  /**
+   * Método que busca recetas por título
+   * @param title : El título a buscar
+   * @param page : El número de página para la paginación (default 1)
+   * @returns El resultado de la búsqueda
+   */
   public searchByTitleRecipes(title: string, page: number=1){
     const params = new HttpParams()
     .set('page', page)
@@ -149,6 +274,12 @@ export class RecipesService {
     return this.http.post("http://localhost:3001/api/v1/recipe/title", body, {'params':params})
   }
 
+  /**
+   * Método que busca recetas por sus ingredientes
+   * @param ingredients : Array de ingredientes por los cual buscar recetas
+   * @param page : El número de página para la paginación (default 1)
+   * @returns El resultado de la búsqueda
+   */
   public searchByIngredientsRecipes(ingredients: number[], page: number=1){
     const params = new HttpParams()
     .set('page', page)
@@ -158,6 +289,10 @@ export class RecipesService {
     return this.http.post("http://localhost:3001/api/v1/recipe/ingredients", body, {'params':params})
   }
 
+  /**
+   * Método que obtiene una receta al azar
+   * @returns {Observable<any>} La receta encontrada al azar
+   */
   public randomRecipe():Observable<any>{
     return this.http.get("http://localhost:3001/api/v1/recipe/search/random");
   }
@@ -187,21 +322,4 @@ export class RecipesService {
         map(recipes => recipes.length)
       );
   }
-
-  /**
-   * Método que recibe una receta nueva y la guarda en la base de datos.
-   * @param {Food} recipe : La nueva receta a crearse y almacenarse.
-   */
-  public addRecipe(recipe: Food) {
-    //add logic
-  }
-
-  /**
-   * Método que recibe una receta ya existente y la guarda en la base de datos.
-   * @param {Food} recipe : La receta editada a almacenarse.
-   */
-  public updateRecipe(recipe: Food) {
-    //add logic
-  }
-
 }
