a260d91159b53be6284d687c59cad3022d6bdf88
Alexandra Oreza <A18016321@alumnos.uady.mx>
2023-05-16 21:47:02 -0600

    missing comments in IngredientService

--- src/app/services/ingredients.service.ts
+++ src/app/services/ingredients.service.ts
@@ -27,7 +27,13 @@ export class IngredientsService {
     return this.http.get("http://localhost:3001/api/v1/ingredient/", {'params': params});
   }
 
-
+  /**
+   * Método que devuelve los ingredientes activos
+   * 
+   * @param token : El token de autorización para autenticación
+   * @param page : El número de páginas para la paginación
+   * @returns {Observable<any>} Los ingredientes activos
+   */
   public getAllIngredientsActivated(token:string, page: number): Observable<any>{
     const params = new HttpParams()
     .set('page', page)
@@ -35,11 +41,24 @@ export class IngredientsService {
     return this.http.get('http://localhost:3001/api/v1/ingredient/', {'headers': headers, 'params': params});
   }
 
+  /**
+   * Método que agrega un nuevo ingrediente a la DB
+   * 
+   * @param token : El token de autorización para autenticación
+   * @param name : El nombre del ingrediente
+   * @returns {Observable<any>} El resultado de la adición
+   */
   public addIngredient(token: string, name: string): Observable<any>{
     const headers = new HttpHeaders({'authorization': token});
     return this.http.post("http://localhost:3001/api/v1/ingredient/", {'name': name},  {'headers': headers});
   }
 
+  /**
+   * Método que elimina un ingrediente de acuerdo a su Id
+   * @param token : El token de autorización para autenticación
+   * @param id : El id del ingrediente a eliminar
+   * @returns {Observable<any>} El resultado de la eliminación
+   */
   public deleteIngredient(token: string, id: number): Observable<any>{
     const headers = new HttpHeaders({'Authorization': token});
     return this.http.delete("http://localhost:3001/api/v1/ingredient/" + id, {headers: headers});
