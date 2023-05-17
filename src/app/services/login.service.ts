37b186c76fb664f3efef15b24d2e4d5ebcb1418b
Alexandra Oreza <A18016321@alumnos.uady.mx>
2023-05-16 21:57:12 -0600

    missing comments in LoginService

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -23,10 +23,21 @@ export class LoginService {
     return this.http.get(this.LOCAL_USERS);
   }
 
+  /**
+   * Método que realiza la autenticación de login
+   * @param email : El correo del usuario
+   * @param password : La contraseña del usuario
+   * @returns {Observable<any>} El resultado de la autenticación
+   */
   public login_auth(email: string, password: string): Observable<any>{
     return this.http.post('http://localhost:3001/api/v1/auth', {email: email, password: password})
   }
 
+  /**
+   * Método que obtiene el tipo la autenticación del usuario
+   * @param token : El token de autorización para autenticación
+   * @returns El tipo de autenticación del usuario
+   */
   public type_auth(token: string): Observable<any>{
     const headers = new HttpHeaders({'authorization': token});
     return this.http.post('http://localhost:3001/api/v1/auth/getInfo', null, { 'headers': headers });
