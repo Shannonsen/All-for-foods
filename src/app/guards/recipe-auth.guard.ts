f14ee56eaade9cbe158686879d390050817edac8
Shannonsen <a18016307@alumnos.uady.mx>
2023-05-17 17:05:15 -0600

    feature: comments guards

--- src/app/guards/recipe-auth.guard.ts
+++ src/app/guards/recipe-auth.guard.ts
@@ -3,34 +3,47 @@ import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTr
 import { CookieService } from 'ngx-cookie-service';
 import { Observable } from 'rxjs';
 import { RecipesService } from '../services/recipes.service';
-
+/**
+ * Clase que representa el middleware de autorización para permitir editar una receta (view recipe), la cual solo puede ser editada por el usuario que la creo.
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeAuthGuard implements CanActivate {
 
-  constructor(private cookieService: CookieService, private recipesService: RecipesService, private router: Router){
+  /**
+   * @constructor
+   * @param {CookieService} cookieService : Servicio de cookies
+   * @param {Router} router : Navegación entre rutas
+   * @param {RecipesService} recipesService : Servicio de recetas
+   */
+  constructor(private cookieService: CookieService, private recipesService: RecipesService, private router: Router) { }
 
-  }
+  /**
+ * Metodo que sirve para permitir o no permitir acceso a una ruta
+ * @param {ActivatedRouteSnapshot} route : Parametro que contiene información sobre los parámetros, datos estáticos y otras propiedades relacionadas con la ruta.
+ * @param {RouterStateSnapshot} state : Parametro que proporciona información sobre la URL actual, los parámetros de ruta, los datos de la ruta y otros detalles relevantes.
+ * @returns
+ */
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
-      var token = this.cookieService.get('Token');
-      var userId = this.cookieService.get('idUser');
-      if (token) {
-        return new Promise<boolean>((resolve) => {
-          this.recipesService.getRecipeById(route.params['id']).subscribe(recipe => {
-            if (recipe.results.user.id == userId) {
-              resolve(true)
-            }else{
-              this.router.navigate(['notfound/']);
-            }
-          })
+    var token = this.cookieService.get('Token');
+    var userId = this.cookieService.get('idUser');
+    if (token) {
+      return new Promise<boolean>((resolve) => {
+        this.recipesService.getRecipeById(route.params['id']).subscribe(recipe => {
+          if (recipe.results.user.id == userId) {
+            resolve(true)
+          } else {
+            this.router.navigate(['notfound/']);
+          }
         })
-      }else{
-        return this.router.navigate(['notfound/']);
-      }
+      })
+    } else {
+      return this.router.navigate(['notfound/']);
+    }
   }
 
 }
