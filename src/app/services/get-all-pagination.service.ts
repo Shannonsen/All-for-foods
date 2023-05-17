1609fdd9f591d3c99dad443f7e4e6b366eb0ff7d
Alexandra Oreza <A18016321@alumnos.uady.mx>
2023-05-16 21:38:28 -0600

    missing comments for GetAllPaginationService

--- src/app/services/get-all-pagination.service.ts
+++ src/app/services/get-all-pagination.service.ts
@@ -6,12 +6,36 @@ import { UserService } from './user.service';
 import { IngredientsService } from './ingredients.service';
 import { CommentService } from './comment.service';
 
+/**
+ * Clase que representa el servicio de paginación.
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class GetAllPaginationService {
+
+  /**
+   * @constructor
+   * @param http : Cliente http
+   * @param recipeService : Servicio de recetas
+   * @param userService : Servicio de usuarios
+   * @param ingredientService : Servicio de ingredientes
+   * @param commentService : Servicio de comentarios
+   */
   constructor(private http: HttpClient, private recipeService: RecipesService, private userService: UserService, private ingredientService: IngredientsService, private commentService: CommentService) { }
 
+  /**
+   * Obtiene información de diferentes servicios basados en un tipo de búsqueda
+   * @param {string} typeSearch : El tipo de búsqueda a realizar
+   * @param {number} page : El número de páginas para la paginación
+   * @param {number} size : El tamaño de items por página (4 por default)
+   * @param {string} token : El token de autorización para autenticación
+   * @param {number} idUser : El id del usuario (si aplica)
+   * @param {number} idRecipe : El id de la receta (si aplica)
+   * @param {string} title : El título de la receta (si aplica)
+   * @param {number[]} ingredients : Array de ids de ingredientes (si aplica)
+   * @returns {Observable<any>} Información basada en la búsqueda
+   */
   public getServiceRecipes(typeSearch:string ="", page:number, size:number = 4, token:string = "", idUser:number , idRecipe: number, title: string, ingredients: number[]): Observable<any>{
     switch(typeSearch){
       case 'delete':
