b075b90d02269d563ef1685005d94c1f243154e0
Alexandra Oreza <A18016321@alumnos.uady.mx>
2023-05-16 21:30:15 -0600

    comments for CommentService

--- src/app/services/comment.service.ts
+++ src/app/services/comment.service.ts
@@ -29,6 +29,13 @@ export class CommentService {
     return this.http.get(this.LOCAL_COMMENTS);
   }
 
+  /**
+   * Método que obtiene todos los comentarios en una receta específica
+   * 
+   * @param idRecipe : el id de la receta
+   * @param page : el número de página para la paginación (default 1)
+   * @returns {Observable<any>} : Lista de comentarios para la receta específica
+   */
   public getAllCommentsSpecificRecipe(idRecipe:number, page: number=1): Observable<any>{
     const params = new HttpParams()
     .set('page', page)
@@ -37,6 +44,10 @@ export class CommentService {
 
 /**
   * Método para añadir comentarios
+  * @param recipeID : el id de la receta
+  * @param userId : el id del usuario al que pertenece el comentario
+  * @param comment : el comentario
+  * @param token : el token del usuario
   * @returns {Observable<any>} Lista de comentarios
   */
   postComment(recipeID:number, userId: number, comment: string, token:string): Observable<any> {
@@ -49,6 +60,13 @@ export class CommentService {
     return this.http.post("http://localhost:3001/api/v1/comment/",body, {'headers':headers})
   }
 
+  /**
+   * Método que elimina un comentario por su id
+   * 
+   * @param idComment : el id del comentario a eliminar
+   * @param token : el token de autorización para autenticación
+   * @returns {Observable<any>} El resultado de la eliminación
+   */
   deleteComment(idComment: number, token:string): Observable<any> {
     const headers = new HttpHeaders({'authorization': token})
     return this.http.delete("http://localhost:3001/api/v1/comment/" + idComment, {'headers':headers})
