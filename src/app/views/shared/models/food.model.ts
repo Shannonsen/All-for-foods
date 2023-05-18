6f7c132085fabc7066fdcd528dcf84f441119e01
Shannonsen <a18016307@alumnos.uady.mx>
2023-05-18 16:55:48 -0600

    feature: comments models

--- src/app/views/shared/models/food.model.ts
+++ src/app/views/shared/models/food.model.ts
@@ -1,15 +1,14 @@
 /**
  * @interface Food
  * @property {number} id : Identificador único de la receta
- * @property {string} name : Nombre de la receta
- * @property {number} author : Identificador único del autor
+ * @property {string} title : Nombre de la receta
+ * @property {number} user : Identificador único del user con su información
  * @property {string[]} ingredients : Arreglo con los nombres de los ingredientes de la receta
  * @property {string} image : Url de la imagen de la receta
- * @property {string} category : Nombre de la categoría a la cual pertenece la receta
- * @property {string} process : Pasos para realizar la receta
- * @property {number} rating : Calificación de la receta
+ * @property {string} steps : Pasos para realizar la receta
+ * @property {number} rate : Calificación de la receta
  * @property {string} description : Descripción de la receta
- * @property {string} creationDate : Fecha de creación de la receta en forma dd-MM-yyyy
+ * @property {string} createdAt : Fecha de creación de la receta en forma dd-MM-yyyy
  * @property {string} lastModified : Fecha de última actualización de la receta en forma dd-MM-yyyy
  */
 export interface Food{
